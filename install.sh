fe86584b5f44d771b92b9857a25aa7caba9259aa
Joe Egan <joe@Joes-MacBook-Air.local>
2018-12-14 13:50:29 -0500

    Added upload and install files

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir ~/.config/nvim
+cp ./init.vim ~/.config/nvim/
+mkdir ~/.vim
+cp -r ./plugins ~/.vim/
---
+++ upload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir ./archive
+rm -rf ./archive/*
+mv ./plugins ./archive
+mv ./init.vim ./archive
+cp .config/nvim/init.vim ./
+cp -r .vim/plugins ./
+git add *
+git commit
+git push
