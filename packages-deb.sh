d4b60c07e65df8f3ec1863771760bb60f14e238b
Joe Egan <joeegan2202@gmail.com>
2019-01-08 14:59:14 -0500

    Added vim plug to package script

--- packages-deb.sh
+++ packages-deb.sh
@@ -18,6 +18,9 @@ apt install -y htop
 apt install -y neovim
 apt install -y zsh
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 cd ~/
