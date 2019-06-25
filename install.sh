9dbd9a627538805bb0021b193d1790e4626d5396
Joe Egan <joeegan2202@gmail.com>
2019-06-25 18:10:24 -0400

    Updated lots of stuff. Should work now

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 git pull
+
+if [ "$(whoami)" != "root" ]; then
+	echo "Sorry, you must be root to install the dotfiles"
+	exit 1
+fi
+
 # rm ~/.config/nvim/init.vim
 # mkdir -p ~/.config/nvim
 # cp ./init.vim ~/.config/nvim/
@@ -9,7 +15,7 @@ git pull
 # rm ~/.zshrc
 # cp ./zsh.config ~/.zshrc
 mkdir -p ~/.config/nvim ~/.config/i3 ~/.config/polybar
-ln -sf neovim/neovim.config ~/.config/nvim/init.vim
-ln -sf zsh/zsh.config ~/.zshrc
-ln -sf i3/i3.config ~/.config/i3/config
-ln -sf polybar/polybar.config ~/.config/polybar/config
+ln -sf ~/dotfiles/neovim/neovim.config ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/zsh/zsh.config ~/.zshrc
+ln -sf ~/dotfiles/i3/i3.config ~/.config/i3/config
+ln -sf ~/dotfiles/polybar/polybar.config ~/.config/polybar/config
