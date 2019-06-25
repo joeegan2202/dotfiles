8999c5976e74043914a3acfa43443da256f9bfa1
Joe Egan <joeegan2202@gmail.com>
2019-06-25 17:42:03 -0400

    Reconfigured for Symlinking!

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 git pull
-rm ~/.config/nvim/init.vim
-mkdir -p ~/.config/nvim
-cp ./init.vim ~/.config/nvim/
-rm ~/.config/i3/config
-mkdir -p ~/.config/i3/
-cp ./i3.config ~/.config/i3/config
-rm ~/.zshrc
-cp ./zsh.config ~/.zshrc
+# rm ~/.config/nvim/init.vim
+# mkdir -p ~/.config/nvim
+# cp ./init.vim ~/.config/nvim/
+# rm ~/.config/i3/config
+# mkdir -p ~/.config/i3/
+# cp ./i3.config ~/.config/i3/config
+# rm ~/.zshrc
+# cp ./zsh.config ~/.zshrc
+mkdir -p ~/.config/nvim ~/.config/i3
+ln -sf neovim/init.vim ~/.config/nvim/init.vim
+ln -sf zsh/zsh.config ~/.zshrc
+ln -sf i3/i3.config ~/.config/i3/config
