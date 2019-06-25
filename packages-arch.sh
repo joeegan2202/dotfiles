e2290677a8613be69320720d471a423e84456383
Joe Egan <joeegan2202@gmail.com>
2019-06-25 19:26:12 -0400

    Fixed all

--- packages-arch.sh
+++ packages-arch.sh
@@ -7,11 +7,7 @@ fi
 
 sudo pacman -Syyui --noconfirm
 
-<<<<<<< HEAD
 sudo pacman -S git base-devel go --noconfirm
-=======
-sudo pacman -S git base-devel go
->>>>>>> c1369c5afc7f95c0275012f261bb160e56332da3
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
