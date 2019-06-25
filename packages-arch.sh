48c5486814512a82c809ed09de2b64e8eb5d2cbd
Joe Egan <joeegan2202@gmail.com>
2019-06-25 19:20:44 -0400

    Updated packages

--- packages-arch.sh
+++ packages-arch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ "$(whoami)" != "root" ]; then
-	echo "Sorry, you must be root to install the dotfiles"
+if [ "$(whoami)" == "root" ]; then
+	echo "Sorry, you must not be root to install the dotfiles"
 	exit 1
 fi
 
-pacman -Syyui --noconfirm
+sudo pacman -Syyui --noconfirm
 
-pacman -S git makepkg go
+sudo pacman -S git base-devel go --noconfirm
 
 git clone https://github.com/jguer/yay
 cd yay
