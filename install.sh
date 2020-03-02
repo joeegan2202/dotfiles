f3f6970c3627e46e90a32682cec5fe17a631d9c9
Joe Egan <joeegan2202@gmail.com>
2020-03-02 16:01:28 -0500

    Changed the install script to include alacritty

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ fi
 # cp ./i3.config ~/.config/i3/config
 # rm ~/.zshrc
 # cp ./zsh.config ~/.zshrc
-sudo mkdir -p ~/.config/nvim ~/.config/i3 ~/.config/polybar ~/.config/ranger
+sudo mkdir -p ~/.config/nvim ~/.config/i3 ~/.config/polybar ~/.config/ranger ~/.config/alacritty
 sudo ln -sf ~/dotfiles/neovim/neovim.config ~/.config/nvim/init.vim
 sudo ln -sf ~/dotfiles/neovim/coc-settings.json ~/.config/nvim/coc-settings.json
 sudo ln -sf ~/dotfiles/zsh/zsh.config ~/.zshrc
@@ -26,6 +26,7 @@ sudo ln -sf ~/dotfiles/i3/i3.config ~/.config/i3/config
 sudo ln -sf ~/dotfiles/polybar/polybar.config ~/.config/polybar/config
 sudo ln -sf ~/dotfiles/Xresources/Xresources.config ~/.Xresources
 sudo ln -sf ~/dotfiles/ssh/ssh.config ~/.ssh/config
+sudo ln -sf ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 sudo ln -sf ~/dotfiles/ranger/commands_full.py ~/.config/ranger/commands_full.py
 sudo ln -sf ~/dotfiles/ranger/commands.py ~/.config/ranger/commands.py
